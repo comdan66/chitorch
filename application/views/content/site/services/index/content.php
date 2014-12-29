6d3dcf91a670ef4aba7eb4d7e765b398d8dd89a2
OA Wu <comdan66@gmail.com>
2014-12-30 07:46:39 +0800

    Temp

--- application/views/content/site/services/index/content.php
+++ application/views/content/site/services/index/content.php
@@ -1,159 +1,242 @@
+
 <div id="RIGHTC">
-  <div id="TOPTITLE">
-    <p class="bigtitle">Service</p>
-  </div>
-
-  <div id="SMATITLE">
-    <p class="contactname">服務項目</p>
-    <p class="sertitle">室內空間整合規劃設計</p>
-  </div>
-
-  <!-- 右邊  內容 START -->
-  <div id="CENTRB">
-    <div id="SERTOP">
-      <div class="SERiconbox">
-        <div class="iconsmbox">
-          <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon01.png'));?>" width="100%">
-        </div>
-        <p class="SN">住宅</p>
-        <p class="ST">設計/施工</p>
+          <!-- 右邊 大標題 START-->
+            <div id="TOPTITLE">
+              <p class="bigtitle">Service</p>
+            </div>
+            <!-- 右邊 大標題 END -->
+            
+            <!-- 右邊 小標題 START -->
+            <div id="SMATITLE">
+              <p class="contactname">服務項目</p>
+                <p class="sertitle">室內空間整合規劃設計</p>
+            </div>
+            <!-- 右邊 小標題 END -->
+            
+            <!-- 右邊  內容 START -->
+            <div id="CENTRB">
+              <div id="SERTOP">
+                  <div class="SERiconbox">
+                      <div class="iconsmbox">
+                        <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon01.png'));?>" width="100%">
+                        </div>
+                        
+                        <p class="SN">住宅</p>
+                        <p class="ST">設計/施工</p>
+                    </div>
+                    
+                    <div class="SERiconbox">
+                      <div class="iconsmbox">
+                        <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon02.png'));?>" width="100%">
+                        </div>
+                        
+                        <p class="SN">透天別墅大宅</p>
+                        <p class="ST">設計/施工</p>
+                    </div>
+                    
+                    <div class="SERiconbox">
+                      <div class="iconsmbox">
+                        <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon03.png'));?>" width="100%">
+                        </div>
+                        
+                        <p class="SN">中古屋翻新</p>
+                        <p class="ST">設計/施工</p>
+                    </div>
+                    
+                    <div class="SERiconbox">
+                      <div class="iconsmbox">
+                        <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon04.png'));?>" width="100%">
+                        </div>
+                        
+                        <p class="SN">辦公室規劃</p>
+                        <p class="ST">設計/施工</p>
+                    </div>
+                    
+                    <div class="SERiconbox">
+                      <div class="iconsmbox">
+                        <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon05.png'));?>" width="100%">
+                        </div>
+                        
+                        <p class="SN">商業空間規劃</p>
+                        <p class="ST">設計/施工</p>
+                    </div>
+                    
+                    
+                </div>
+        
+                <div class="SERbox">
+                  <div id="simgbox">
+                    <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'service_image.jpg'));?>" width="100%">
+                    </div>
+                </div>
+                
+                <div class="SERbox">
+                  <div id="SERDetitle">收費標準</div>
+                    <div class="KKK">
+                       <p class="sercgre">講求材質運用與施工品質，藉由專案方式完整表現每個細節，替空間氛圍開創更高的價值。</p>
+                    </div>
+                    
+                    <div class="SERCENbox">
+                      <div class="SERCENboxtitle">設計費用：<span class="sercgre">若承接工程，設計費折扣2000元</span>
+                        </div>
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon10.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">預售屋【客變】</p>
+                                <ul>
+                                    <li class="FFF">10坪以下：依5萬元計</li>
+                                    <li class="FFF">10坪以上：依5000/坪計</li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon6.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">新成屋</p>
+                                <ul>
+                                    <li class="FFF">10坪以下：依5萬元計</li>
+                                    <li class="FFF">10坪以上：依5000/坪計</li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon7.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">辦公室</p>
+                                <ul>
+                                    <li class="FFF">依個案實際情況報價</li>
+                                    <li class="HHH">依坪數大小、設計複雜程度報價</li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon9.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">商業空間</p>
+                                <ul>
+                                    <li class="FFF">依個案實際情況報價</li>
+                                    <li class="HHH">依坪數大小、設計複雜程度報價</li>
+                                </ul>
+                            </div>
+                        </div> 
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon8.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">中古屋【舊屋翻新】</p>
+                                <ul>
+                                    <li class="FFF">10坪以下：依5萬元計</li>
+                                    <li class="FFF">10坪以上：依5000/坪計</li>
+                                </ul>
+                            </div>
+                        </div>
+                    
+                        
+                    </div>   
+                    
+                    <div class="SERCENbox">
+                      <div class="SERCENboxtitle">
+                        工程費用：<span class="sercgre">施工單價以每坪概算</span>
+                        </div>
+                        
+                        
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon8.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">中古屋【舊屋翻新】</p>
+                                <ul>
+                                    <li class="FFF">約8~12萬元或更高價位</li>
+                                    <li class="HHH">依個案施工難易、數量多寡差異不同</li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon7.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">辦公室</p>
+                                <ul>
+                                    <li class="FFF">約2~5萬元或更高價位</li>
+                                    <li class="HHH">依個案施工難易、數量多寡差異不同</li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon9.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">商業空間</p>
+                                <ul>
+                                    <li class="FFF">依個案實際情況報價</li>
+                                    <li class="HHH">依坪數大小、設計複雜程度報價</li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                        <div class="cenBOX">
+                          <div class="BOXimg">
+                              <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon6.png'));?>" width="100%">
+                            </div>
+                            
+                            <div class="typebox">
+                                <p class="smallboxtype">預售/新成屋 </p>
+                                <ul>
+                                    <li class="FFF">約6~8萬元或更高價位</li>
+                                    <li class="FFF">無更動格局、中等以上質感，約5萬元以上</li>
+                                    <li class="HHH">依個案施工難易、數量多寡差異不同</li>
+                                </ul>
+                            </div>
+                        </div>
+                         
+                    </div>
+                    
+                    <div class="SERCENbox">
+                      <div class="SERCENboxtitle">
+                        監工管理費：<span class="sercgre">工程總價8~10%(視個案距離而定)</span>
+                        </div>
+                        
+                        <div class="typebox">
+                           <div class="KKKD">
+                           <p class="sercgre">備註：</br>
+                           01. 以上收費標準僅供參考，實際價格依個案現場狀況調整。</br>
+                           02. 詳細個案估價內容依實際設計內容、實際單價尺寸、材料等明細為主。實作實算，責任施工。</br>
+                           03. 礙於對品質的堅持，恕不接受局部設計/施工。</p>
+                           </div>
+                        </div>  
+                    </div>     
+                </div>
+              
+            </div>
+            <!-- 右邊  內容 END -->
       </div>
-
-      <div class="SERiconbox">
-        <div class="iconsmbox">
-          <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon02.png'));?>" width="100%">
-        </div>
-
-        <p class="SN">透天別墅大宅</p>
-        <p class="ST">設計/施工</p>
-      </div>
-
-      <div class="SERiconbox">
-        <div class="iconsmbox">
-          <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon03.png'));?>" width="100%">
-        </div>
-
-        <p class="SN">中古屋翻新</p>
-        <p class="ST">設計/施工</p>
-      </div>
-
-      <div class="SERiconbox">
-        <div class="iconsmbox">
-          <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon04.png'));?>" width="100%">
-        </div>
-
-        <p class="SN">辦公室規劃</p>
-        <p class="ST">設計/施工</p>
-      </div>
-
-      <div class="SERiconbox">
-        <div class="iconsmbox">
-          <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon05.png'));?>" width="100%">
-        </div>
-
-        <p class="SN">商業空間規劃</p>
-        <p class="ST">設計/施工</p>
-      </div>
-    </div>
-
-    <div class="SERbox">
-      <div id="simgbox">
-        <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'service_image.jpg'));?>" width="100%">
-      </div>
-    </div>
-
-    <div class="SERbox">
-      <div id="SERDetitle">收費標準</div>
-
-      <div class="SERCENbox">
-        <div class="SERCENboxtitle">設計與監管費：</div>
-
-        <div class="typebox">
-          <div class="KKK">
-            <p class="sercgre">住家:4000/坪</p>
-            <p class="sercgre">商業空間:2000-3000/坪</p>
-            <p class="sercgre">監管費用:總工程款的8-10%</p>
-          </div>
-        </div>  
-      </div>   
-
-      <div class="SERCENbox">
-        <div class="SERCENboxtitle">
-          工程費用：<span class="sercgre">施工單價以每坪概算</span>
-        </div>
-
-        <div class="cenBOX">
-          <div class="BOXimg">
-            <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon6.png'));?>" width="100%">
-          </div>
-
-          <div class="typebox">
-            <p class="smallboxtype">預售/新成屋 </p>
-            <ul>
-              <li class="FFF">約6~8萬元或更高價位</li>
-              <li class="FFF">無更動格局、中等以上質感，約5萬元以上</li>
-              <li class="HHH">依個案施工難易、數量多寡差異不同</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="cenBOX">
-          <div class="BOXimg">
-            <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon7.png'));?>" width="100%">
-          </div>
-
-          <div class="typebox">
-            <p class="smallboxtype">辦公室</p>
-            <ul>
-              <li class="FFF">約2~5萬元或更高價位</li>
-              <li class="HHH">依個案施工難易、數量多寡差異不同</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="cenBOX">
-          <div class="BOXimg">
-            <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon8.png'));?>" width="100%">
-          </div>
-
-          <div class="typebox">
-            <p class="smallboxtype">中古屋【舊屋翻新】</p>
-            <ul>
-              <li class="FFF">約8~12萬元或更高價位</li>
-              <li class="HHH">依個案施工難易、數量多寡差異不同</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="cenBOX">
-          <div class="BOXimg">
-            <img src="<?php echo base_url (array ('resource', 'site', 'images', 'service', 'icon9.png'));?>" width="100%">
-          </div>
-
-          <div class="typebox">
-            <p class="smallboxtype">商業空間</p>
-            <ul>
-              <li class="FFF">依個案實際情況報價</li>
-              <li class="HHH">依坪數大小、設計複雜程度報價</li>
-            </ul>
-          </div>
-        </div> 
-      </div>
-
-      <div class="SERCENbox">
-        <div class="SERCENboxtitle">
-          監工管理費：<span class="sercgre">工程總價8~10%(視個案距離而定)</span>
-        </div>
-
-        <div class="typebox">
-          <div class="KKK">
-            <p class="sercgre">備註：</p>
-            <p class="sercgre">01. 以上收費標準僅供參考，實際價格依個案現場狀況調整。</p>
-            <p class="sercgre">02. 詳細個案估價內容依實際設計內容、實際單價尺寸、材料等明細為主。實作實算，責任施工。</p>
-            <p class="sercgre">03. 礙於對品質的堅持，恕不接受局部設計/施工。</p>
-          </div>
-        </div>  
-      </div>     
-    </div>
-  </div>
-</div>
